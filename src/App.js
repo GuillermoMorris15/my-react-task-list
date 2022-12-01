a83f3450b053e1565ae3e7cf2e5c2aa3a7598a22
Guillermo Morris <gmorris.training@wowdesarrollos.com>
2022-12-01 09:36:17 -1100

    corregi errores que tenia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Titulo from './componentes/Header/header';
-import ListaTareas from './componentes/ListaTareas/ListaTareas';
+import ListaTareas from './componentes/ListaTareas/listaTareas';
 import Footer from './componentes/footer/Footer';
 
 function App() {
