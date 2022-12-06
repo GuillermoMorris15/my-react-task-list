d0bd3612eaf584cd7becc73deac799c1ed0a4ba7
Guillermo Morris <gmorris.training@wowdesarrollos.com>
2022-12-05 14:50:16 -1100

    agregue nuevos cambios a mi lista

---
+++ src/componentes/SobreNosotros.jsx
@@ -0,0 +1,24 @@
+import Menu from "./menu/Menu"
+function SobreNosotros () {
+    return(
+        <div>
+            <Menu/>
+            <h2>Sobre Nosotros</h2>
+            <strong>Descripcion</strong>
+            <p>listas de tareas es una forma facil y divertida de escribir y realixar todas tus tareas.</p>
+            <strong>Funciones</strong>
+            <ol>
+                <li>Guarda edita muestra y elimina tareas.</li>
+                <li>las tareas pueden cambiar y variar segun tu gusto.</li>
+                <li>puedes saber la cantidad de tareas que tienes pendiente.</li>
+                <li>Es muy facil de usar.</li>
+            </ol>
+            <strong>Desarrollado en JavaScrip</strong> <br/>
+            <strong> Tambien uso de React</strong>
+
+
+        </div>
+    )
+}
+
+export default SobreNosotros
