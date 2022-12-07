1266bf41d345d7a29080a6cd328aaa9ef298467d
Guillermo Morris <gmorris.training@wowdesarrollos.com>
2022-12-07 10:32:51 -1100

    cambio en la lista

--- src/componentes/SobreNosotros.jsx
+++ src/componentes/SobreNosotros.jsx
@@ -1,3 +1,4 @@
+import { OrderedList } from "@chakra-ui/react"
 import Menu from "./menu/Menu"
 // import {
 //     ListItem,
@@ -13,13 +14,14 @@ function SobreNosotros () {
             <strong>Descripcion</strong>
             <p>listas de tareas es una forma facil y divertida de escribir y realixar todas tus tareas.</p>
             <strong>Funciones</strong>
+            <OrderedList>
             <ol>
                 <li>Guarda edita muestra y elimina tareas.</li>
                 <li>las tareas pueden cambiar y variar segun tu gusto.</li>
                 <li>puedes saber la cantidad de tareas que tienes pendiente.</li>
                 <li>Es muy facil de usar.</li>
             </ol>
-            
+            </OrderedList>
 
 
         </div>
